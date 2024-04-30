5cde58920f14de0da12425be11d11a44a6322f82
Purple-Code-sh <purple.code.sh@gmail.com>
2024-04-30 17:32:18 -0600

    adds a personalized font to Movies section, using google fonts resource integrated at next

--- src/app/movies/layout.tsx
+++ src/app/movies/layout.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import { Comfortaa } from "next/font/google";
 
 export const metadata = {
   title: "Movies Section - SHM",
   description: "Section dedicated to rate, search and watch movies",
   keywords: "movies, peliculas, ver, streaming",
 };
+
+const confortaFont = Comfortaa({
+  weight: ["300", "400", "500", "600", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 export default function MoviesLayout({
   children,
@@ -13,7 +20,7 @@ export default function MoviesLayout({
 }) {
   return (
     <>
-      <nav>
+      <nav className={confortaFont.className}>
         <h1>Welcome to the cinema</h1>
         <p>Recomendation of the day</p>
         <ul>
